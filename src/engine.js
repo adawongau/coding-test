cf610f3e884982921d623074df58d13f827698ea
Dinith Fonseka <dinith.fonseka@vmlyr.com>
2022-03-10 23:53:50 +0800

    code implementation covers followings: 1. Fetch json file and adding data to select element in html 2. adding event listeners to capture selected item. 3. define timers, clear timers 4. class object initialisation and pass data to convert string data set to string array 5. pass string array to convert into boolean data array 6. run the algorithm and work on the conditions mentioned 7. update dataset according to the conditions and submit back to print 8. print the data into canvas and achieve  final output 9. adding CSS simple styling for view 10. covering test case scenarios according to the mocha test scripts to full fill the edge cases

--- src/engine.js
+++ src/engine.js
@@ -1,8 +1,16 @@
 // (world: boolean[][]) => boolean[][]
+import DieOrLive from './DieOrLive.js';
 export const next = (world) => {
-  return world;
+  let dol = new DieOrLive()
+  dol.constructArr(world)
+  dol.updateDataSets();
+  let genArr =dol.fillingArr();
+  return genArr;
 };
 // (pattern: string) => boolean[][]
 export const parse = (pattern) => {
-  return [];
+  let dol = new DieOrLive()
+  let rowData = dol.patternToArray(pattern)
+  let dataSet = dol.converStringDatatoArr(rowData)
+  return dataSet;
 };
